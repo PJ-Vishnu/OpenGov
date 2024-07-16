5c00845862315bc42e66dea141afcae487cc5137
PJ-Vishnu <65999631+PJ-Vishnu@users.noreply.github.com>
2024-07-16 16:05:06 +0530

    trying to host

--- vite.config.js
+++ vite.config.js
@@ -1,25 +1,25 @@
-import { defineConfig, normalizePath } from 'vite'
-import react from '@vitejs/plugin-react'
-import path from 'node:path';
-import { createRequire } from 'node:module';
-import { viteStaticCopy } from 'vite-plugin-static-copy'
-const require = createRequire(import.meta.url);
-const cMapsDir = normalizePath(
-  path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps')
-);
+import { defineConfig, normalizePath } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
+import { createRequire } from 'module';
+import { viteStaticCopy } from 'vite-plugin-static-copy';
 
-// https://vitejs.dev/config/
+const require = createRequire(import.meta.url);
+const cMapsDir = normalizePath(path.join(path.dirname(require.resolve('pdfjs-dist/package.json')), 'cmaps'));
+
 export default defineConfig({
-  plugins: [react(),
-  viteStaticCopy({
-    targets: [
-      {
-        src: cMapsDir,
-        dest: 'http://localhost:4000/',
-      },
-    ],
-  }),],
+  plugins: [
+    react(),
+    viteStaticCopy({
+      targets: [
+        {
+          src: cMapsDir,
+          dest: 'build', // Adjust destination path as per your project structure
+        },
+      ],
+    }),
+  ],
   server: {
-    port: 3000
-  }
-})
+    port: 3000,
+  },
+});
